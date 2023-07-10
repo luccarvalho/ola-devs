11e81e8c39c2dcd5964efc87a5b3d6b0e0e0edfd
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-07-10 17:11:25 -0300

    Alteração de código para utilização de rotas aninhadas.

--- src/paginas/Inicio/index.jsx
+++ src/paginas/Inicio/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import Banner from "componentes/Banner";
 import styles from "./Inicio.module.css";
 
 import posts from "json/posts.json";
@@ -7,15 +6,12 @@ import Post from "componentes/Post";
 
 export default function Inicio() {
   return (
-    <main>
-      <Banner />
-      <ul className={styles.posts}>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <Post post={post} />
-          </li>
-        ))}
-      </ul>
-    </main>
+    <ul className={styles.posts}>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <Post post={post} />
+        </li>
+      ))}
+    </ul>
   );
 }
