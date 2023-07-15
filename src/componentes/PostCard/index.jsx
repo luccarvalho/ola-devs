8e7e6591207401d1585a8677831200286e7c91f6
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-07-15 14:53:12 -0300

    Envolvendo toda a div do componente PostCard em um Link.

--- src/componentes/PostCard/index.jsx
+++ src/componentes/PostCard/index.jsx
@@ -1,17 +1,20 @@
+import { Link } from "react-router-dom";
 import styles from "./PostCard.module.css";
 
 import React from "react";
 
 export default function PostCard({ post }) {
   return (
-    <div className={styles.post}>
-      <img
-        className={styles.capa}
-        src={`/assets/posts/${post.id}/capa.png`}
-        alt="Imagem de capa do post"
-      />
-      <h2 className={styles.titulo}>{post.titulo}</h2>
-      <button className={styles.botaoLer}>Ler</button>
-    </div>
+    <Link to={`/posts/${post.id}`}>
+      <div className={styles.post}>
+        <img
+          className={styles.capa}
+          src={`/assets/posts/${post.id}/capa.png`}
+          alt="Imagem de capa do post"
+        />
+        <h2 className={styles.titulo}>{post.titulo}</h2>
+        <button className={styles.botaoLer}>Ler</button>
+      </div>
+    </Link>
   );
 }
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inicio from "./paginas/Inicio";
-import SobreMim from "./paginas/Inicio/SobreMim";
+import SobreMim from "./paginas/SobreMim";
 import Menu from "./componentes/Menu";
 import Rodape from "componentes/Rodape";
 import PaginaPadrao from "componentes/PaginaPadrao";
+import Post from "paginas/Post";
 
 function AppRoutes() {
   return (
@@ -14,6 +15,7 @@ function AppRoutes() {
         <Route path="/" element={<PaginaPadrao />}>
           <Route index element={<Inicio />} />
           <Route path="sobremim" element={<SobreMim />} />
+          <Route path="posts/:id" element={<Post />} />
         </Route>
 
         <Route path="*" element={<div>Página não encontrada!</div>} />
