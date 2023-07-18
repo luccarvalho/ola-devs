f9360438d23d8c65f51a28a820131258b19a0956
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-07-18 12:52:34 -0300

    Tratando Post não encontrado.

--- src/paginas/Post/index.jsx
+++ src/paginas/Post/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
-import { useParams } from "react-router";
+import { Routes, Route, useParams } from "react-router";
 import posts from "json/posts.json";
 import PostModelo from "componentes/PostModelo";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import "./Post.css";
+import NaoEncontrada from "paginas/NaoEncontrada";
+import PaginaPadrao from "componentes/PaginaPadrao";
 
 export default function Post() {
   const parametros = useParams();
@@ -13,17 +15,26 @@ export default function Post() {
   });
 
   if (!post) {
-    return <h1>Post não encontrado...</h1>;
+    return <NaoEncontrada />;
   }
 
   return (
-    <PostModelo
-      fotoCapa={`/assets/posts/${post.id}/capa.png`}
-      titulo={post.titulo}
-    >
-      <div className="post-markdown-container">
-        <ReactMarkdown>{post.texto}</ReactMarkdown>
-      </div>
-    </PostModelo>
+    <Routes>
+      <Route path="*" element={<PaginaPadrao />}>
+        <Route
+          index
+          element={
+            <PostModelo
+              fotoCapa={`/assets/posts/${post.id}/capa.png`}
+              titulo={post.titulo}
+            >
+              <div className="post-markdown-container">
+                <ReactMarkdown>{post.texto}</ReactMarkdown>
+              </div>
+            </PostModelo>
+          }
+        />
+      </Route>
+    </Routes>
   );
 }
