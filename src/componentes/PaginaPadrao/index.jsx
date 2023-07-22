7d95c98faaac13b96180e5ac0308e4ac5a8a2f8f
Lucas Carvalho <lucas.dcarvalho@outlook.com>
2023-07-22 13:36:45 -0300

    Criação do componente Página Padrão.

---
+++ src/componentes/PaginaPadrao/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Banner from "componentes/Banner";
+import { Outlet } from "react-router";
+
+export default function PaginaPadrao() {
+  return (
+    <main>
+      <Banner />
+      <Outlet />
+    </main>
+  );
+}
